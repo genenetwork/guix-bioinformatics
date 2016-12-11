0e304882958494a49bbb74616f907649fffc2a44
pjotrp <pjotr.public01@thebird.nl>
2016-12-11 07:22:53 +0000

    ldc: remove 0.16

--- gn/packages/ldc.scm
+++ gn/packages/ldc.scm
@@ -73,102 +73,6 @@ minimize the amount of work necessary.  Unlike make, rdmd tracks dependencies
 and freshness without requiring additional information from the user.")
       (license license:boost1.0))))
 
-(define-public ldc-0.16.1
-  (package
-    (name "ldc")
-    (version "0.16.1")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append
-                    "https://github.com/ldc-developers/ldc/archive/v"
-                    version ".tar.gz"))
-              (file-name (string-append name "-" version ".tar.gz"))
-              (sha256
-               (base32
-                "1jvilxx0rpqmkbja4m69fhd5g09697xq7vyqp2hz4hvxmmmv4j40"))))
-    (build-system cmake-build-system)
-    ;; LDC currently only supports the x86_64 and i686 architectures.
-    (supported-systems '("x86_64-linux" "i686-linux"))
-    (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (add-after 'unpack 'unpack-submodule-sources
-           (lambda* (#:key inputs #:allow-other-keys)
-             (let ((unpack (lambda (source target)
-                             (with-directory-excursion target
-                               (zero? (system* "tar" "xvf"
-                                               (assoc-ref inputs source)
-                                               "--strip-components=1"))))))
-               (and (unpack "phobos-src" "runtime/phobos")
-                    (unpack "druntime-src" "runtime/druntime")
-                    (unpack "dmd-testsuite-src" "tests/d2/dmd-testsuite")))))
-         (add-after 'unpack-submodule-sources 'patch-dmd2
-           (lambda* (#:key inputs #:allow-other-keys)
-             (substitute* "dmd2/root/port.c"
-               ((" ::isnan") " isnan")
-               ((" ::isinf") " isinf")
-               (("#undef isnan") "")
-               (("#undef isinf") ""))
-             #t))
-         (add-after 'unpack-submodule-sources 'patch-phobos
-           (lambda* (#:key inputs #:allow-other-keys)
-             (substitute* "runtime/phobos/std/process.d"
-               (("/bin/sh") (which "sh"))
-               (("echo") (which "echo")))
-             (substitute* "runtime/phobos/std/datetime.d"
-               (("/usr/share/zoneinfo/")
-                (string-append (assoc-ref inputs "tzdata") "/share/zoneinfo")))
-             (substitute* "tests/d2/dmd-testsuite/Makefile"
-               (("/bin/bash") (which "bash")))
-             #t)))))
-    (inputs
-     `(("libconfig" ,libconfig)
-       ("libedit" ,libedit)
-       ("tzdata" ,tzdata)
-       ("zlib" ,zlib)))
-    (native-inputs
-     `(("llvm" ,llvm-3.7)
-       ("clang" ,clang-3.7)
-       ("unzip" ,unzip)
-       ("phobos-src"
-        ,(origin
-           (method url-fetch)
-           (uri (string-append
-                 "https://github.com/ldc-developers/phobos/archive/ldc-v"
-                 version ".tar.gz"))
-           (sha256
-            (base32
-             "0sgdj0536c4nb118yiw1f8lqy5d3g3lpg9l99l165lk9xy45l9z4"))
-           (patches (search-patches "ldc-disable-tests.patch"))))
-       ("druntime-src"
-        ,(origin
-           (method url-fetch)
-           (uri (string-append
-                 "https://github.com/ldc-developers/druntime/archive/ldc-v"
-                 version ".tar.gz"))
-           (sha256
-            (base32
-             "0z4mkyddx6c4sy1vqgqvavz55083dsxws681qkh93jh1rpby9yg6"))))
-       ("dmd-testsuite-src"
-        ,(origin
-           (method url-fetch)
-           (uri (string-append
-                 "https://github.com/ldc-developers/dmd-testsuite/archive/ldc-v"
-                 version ".tar.gz"))
-           (sha256
-            (base32
-             "0yc6miidzgl9k33ygk7xcppmfd6kivqj02cvv4fmkbs3qz4yy3z1"))))))
-    (home-page "http://wiki.dlang.org/LDC")
-    (synopsis "LLVM compiler for the D programming language")
-    (description
-     "LDC is a compiler for the D programming language.  It is based on the
-latest DMD frontend and uses LLVM as backend.")
-    ;; Most of the code is released under BSD-3, except for code originally
-    ;; written for GDC, which is released under GPLv2+, and the DMD frontend,
-    ;; which is released under the "Boost Software License version 1.0".
-    (license (list license:bsd-3
-                   license:gpl2+
-                   license:boost1.0))))
 
 (define-public ldc-0.17.2
   (package
@@ -218,8 +122,8 @@ latest DMD frontend and uses LLVM as backend.")
              #t)))
        #:tests? #f))
     (native-inputs
-     `(("llvm" ,llvm-3.7)
-       ("clang" ,clang-3.7)
+     `(("llvm" ,llvm-3.7.1)
+       ("clang" ,clang-3.7.1)
        ("unzip" ,unzip)
        ("phobos-src"
         ,(origin
