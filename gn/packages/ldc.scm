e9a935845768ed2ab84810884a2ad516a1f5933a
pjotrp <pjotr.public01@thebird.nl>
2016-12-26 06:20:32 +0000

    Updating ldc to beta6 and renaming source files

--- gn/packages/ldc.scm
+++ gn/packages/ldc.scm
@@ -91,6 +91,7 @@
            (uri (string-append
                  "https://github.com/ldc-developers/phobos/archive/ldc-v"
                  version ".tar.gz"))
+           (file-name (string-append "ldc-phobos-" version ".tar.gz"))
            (sha256
             (base32
              "07hh3ic3r755mq9hn9gfr0wlc5y8cr91xz2ydb6gqy4zy8jgp5s9"))))
@@ -100,6 +101,7 @@
            (uri (string-append
                  "https://github.com/ldc-developers/druntime/archive/ldc-v"
                  version ".tar.gz"))
+           (file-name (string-append "ldc-druntime-" version ".tar.gz"))
            (sha256
             (base32
              "1m1dhday9dl3s04njmd29z7ism2xn2ksb9qlrwzykdgz27b3dk6x"))))
@@ -109,84 +111,88 @@
            (uri (string-append
                  "https://github.com/ldc-developers/dmd-testsuite/archive/ldc-v"
                  version ".tar.gz"))
+           (file-name (string-append "testsuite-" version ".tar.gz"))
            (sha256
             (base32
              "0n7gvalxwfmia4gag53r9qhcnk2cqrw3n4icj1yri0zkgc27pm60"))))))))
 
-(define-public ldc-1.1.0-beta4
-  (package
-    (inherit ldcmain:ldc)
-    (name "ldc")
-    (version "1.1.0-beta4")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append
-                    "https://github.com/ldc-developers/ldc/archive/v"
-                    version ".tar.gz"))
-              (file-name (string-append name "-" version ".tar.gz"))
-              (sha256
-               (base32
-                "04n6sbz6l658s32f7zilpbi9m8a1m4g5wrgl44igkvla30niy5yn"))))
-    (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (add-after 'unpack 'unpack-submodule-sources
-           (lambda* (#:key inputs #:allow-other-keys)
-             (let ((unpack (lambda (source target)
-                             (with-directory-excursion target
-                               (zero? (system* "tar" "xvf"
-                                               (assoc-ref inputs source)
-                                               "--strip-components=1"))))))
-               (and (unpack "phobos-src" "runtime/phobos")
-                    (unpack "druntime-src" "runtime/druntime")
-                    (unpack "dmd-testsuite-src" "tests/d2/dmd-testsuite")))))
-         (add-after 'unpack-submodule-sources 'patch-phobos
-           (lambda* (#:key inputs #:allow-other-keys)
-             (substitute* "runtime/phobos/std/process.d"
-               (("/bin/sh") (which "sh"))
-               (("echo") (which "echo")))
-             (substitute* "runtime/phobos/std/datetime.d"
-               (("/usr/share/zoneinfo/")
-                (string-append (assoc-ref inputs "tzdata") "/share/zoneinfo")))
-             (substitute* "tests/d2/dmd-testsuite/Makefile"
-               (("/bin/bash") (which "bash")))
-             #t)))
-       #:tests? #f))
-    (native-inputs
-     `(("llvm" ,llvm-3.7)
-       ("ldc" ,ldc-0.17.2)
-       ("zlib" ,zlib)
-       ("phobos-src"
-        ,(origin
-           (method url-fetch)
-           (uri (string-append
-                 "https://github.com/ldc-developers/phobos/archive/ldc-v"
-                 version ".tar.gz"))
-           (sha256
-            (base32
-             "1iwy5rs0rqkicj1zfsa5yqvk8ard99bfr8g69qmhlbzb98q0kpks"))))
-       ("druntime-src"
-        ,(origin
-           (method url-fetch)
-           (uri (string-append
-                 "https://github.com/ldc-developers/druntime/archive/ldc-v"
-                 version ".tar.gz"))
-           (sha256
-            (base32
-             "1qsiw5lz1pr8ms9myjf8b94nqi7f1781k226jvxwnhkjg11d0s63"))))
-       ("dmd-testsuite-src"
-        ,(origin
-           (method url-fetch)
-           (uri (string-append
-                 "https://github.com/ldc-developers/dmd-testsuite/archive/ldc-v"
-                 version ".tar.gz"))
-           (sha256
-            (base32
-             "0jp54hyi75i9g41rvgmm3zg21yzv57q8dghrhb432rb0n9j15mbp"))))))))
+(define-public ldc-1.1.0-beta6
+  (let ((version2 "1.1.0-beta4")) ; libraries have not been updated
+    (package
+      (inherit ldcmain:ldc)
+      (name "ldc")
+      (version "1.1.0-beta6")
+      (source (origin
+                (method url-fetch)
+                (uri (string-append
+                      "https://github.com/ldc-developers/ldc/archive/v"
+                      version ".tar.gz"))
+                (file-name (string-append name "-" version ".tar.gz"))
+                (sha256
+                 (base32
+                  "0n53brlkm86jjkppy9xmzx7nyxykzj68kcxgv8q7d10s5hfscxs8"))))
+      (arguments
+       `(#:phases
+         (modify-phases %standard-phases
+           (add-after 'unpack 'unpack-submodule-sources
+             (lambda* (#:key inputs #:allow-other-keys)
+               (let ((unpack (lambda (source target)
+                               (with-directory-excursion target
+                                 (zero? (system* "tar" "xvf"
+                                                 (assoc-ref inputs source)
+                                                 "--strip-components=1"))))))
+                 (and (unpack "phobos-src" "runtime/phobos")
+                      (unpack "druntime-src" "runtime/druntime")
+                      (unpack "dmd-testsuite-src" "tests/d2/dmd-testsuite")))))
+           (add-after 'unpack-submodule-sources 'patch-phobos
+             (lambda* (#:key inputs #:allow-other-keys)
+               (substitute* "runtime/phobos/std/process.d"
+                 (("/bin/sh") (which "sh"))
+                 (("echo") (which "echo")))
+               (substitute* "runtime/phobos/std/datetime.d"
+                 (("/usr/share/zoneinfo/")
+                  (string-append (assoc-ref inputs "tzdata") "/share/zoneinfo")))
+               (substitute* "tests/d2/dmd-testsuite/Makefile"
+                 (("/bin/bash") (which "bash")))
+               #t)))
+         #:tests? #f))
+      (native-inputs
+       `(("llvm" ,llvm-3.7)
+         ("ldc" ,ldc-0.17.2)
+         ("zlib" ,zlib)
+         ("phobos-src"
+          ,(origin
+             (method url-fetch)
+             (uri (string-append
+                   "https://github.com/ldc-developers/phobos/archive/ldc-v"
+                   version2 ".tar.gz")) ; older version
+             (file-name (string-append "ldc-phobos-" version2 ".tar.gz"))
+             (sha256
+              (base32
+               "1iwy5rs0rqkicj1zfsa5yqvk8ard99bfr8g69qmhlbzb98q0kpks"))))
+         ("druntime-src"
+          ,(origin
+             (method url-fetch)
+             (uri (string-append
+                   "https://github.com/ldc-developers/druntime/archive/ldc-v"
+                   version2 ".tar.gz"))
+             (file-name (string-append "ldc-druntime-" version2 ".tar.gz"))
+             (sha256
+              (base32
+               "1qsiw5lz1pr8ms9myjf8b94nqi7f1781k226jvxwnhkjg11d0s63"))))
+         ("dmd-testsuite-src"
+          ,(origin
+             (method url-fetch)
+             (uri (string-append
+                   "https://github.com/ldc-developers/dmd-testsuite/archive/ldc-v"
+                   version2 ".tar.gz")) ; version is behind
+             (file-name (string-append "dmd-testsuite-" version2 ".tar.gz"))
+             (sha256
+              (base32
+               "0jp54hyi75i9g41rvgmm3zg21yzv57q8dghrhb432rb0n9j15mbp")))))))))
 
-(define-public ldc ldc-1.1.0-beta4)
+(define-public ldc ldc-1.1.0-beta6)
 
-;; https://github.com/dlang/tools/archive/v2.072.1.tar.gz
 (define-public rdmd
     (package
       (name "rdmd")
