728073454851da222401d6ad283762c8307fcf96
pjotrp <pjotr.public01@thebird.nl>
2017-02-26 09:01:03 +0000

    Indentation

--- gn/packages/shell.scm
+++ gn/packages/shell.scm
@@ -13,35 +13,33 @@
       (name "shunit2")
       (version (string-append "2.0.4-" (string-take commit 7)))
       (source (origin
-        (method git-fetch)
-        (uri (git-reference
-          (url "https://github.com/kward/shunit2.git")
-          (commit commit)))
-        (file-name (string-append name "-" version "-checkout"))
-        (sha256
-         (base32 "11savxc6qliqv25kv59qak6j7syjv95hbpmq1szn1mzn32g2gc25"))))
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/kward/shunit2.git")
+                      (commit commit)))
+                (file-name (string-append name "-" version "-checkout"))
+                (sha256
+                 (base32 "11savxc6qliqv25kv59qak6j7syjv95hbpmq1szn1mzn32g2gc25"))))
       (build-system gnu-build-system)
       (arguments
        `(
-         #:tests? #f ;; no tests-suite
+         #:tests? #f ;; no test-suite
          #:phases
          (modify-phases %standard-phases
-                        (delete 'configure)
-                        (delete 'build)
-                        (replace 'install
-                                 (lambda* (#:key outputs #:allow-other-keys)
-                                   (let* ((out (assoc-ref outputs "out"))
-                                          (bin (string-append out "/bin"))
-                                          (shunit2-exec (string-append bin "/shunit2"))
-                                          )
-                                     (write (file-exists? "source/2.0/src/shell/shunit2"))
-                                     (write (format #t "build directory: ~s~%" (getcwd)))
-                                     (mkdir-p bin)
-                                     (copy-file "source/2.0/src/shell/shunit2" shunit2-exec)
-                                     (chmod shunit2-exec #o555)
-                                     #t
-                                          ))))
-           ))
+           (delete 'configure)
+           (delete 'build)
+           (replace 'install
+             (lambda* (#:key outputs #:allow-other-keys)
+               (let* ((out (assoc-ref outputs "out"))
+                      (bin (string-append out "/bin"))
+                      (shunit2-exec (string-append bin "/shunit2")))
+                 (write (file-exists? "source/2.0/src/shell/shunit2"))
+                 (write (format #t "build directory: ~s~%" (getcwd)))
+                 (mkdir-p bin)
+                 (copy-file "source/2.0/src/shell/shunit2" shunit2-exec)
+                 (chmod shunit2-exec #o555)
+                 #t))))
+         ))
       (home-page "https://code.google.com/archive/p/shunit2/")
       (synopsis "xUnit based unit testing for Unix shell scripts")
       (description
