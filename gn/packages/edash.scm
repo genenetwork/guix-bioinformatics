377109d375c94eec18073c7260e63ca31d3773b6
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2018-09-24 14:19:41 +0300

    gn: New variables. New dependencies
    * gn/packages/edash.scm (python-speaklater,python-flask-mail)
      New variables.
    
    * gn/packages/edash.scm (edash): Add dependencies.

--- gn/packages/edash.scm
+++ gn/packages/edash.scm
@@ -7,8 +7,10 @@
   #:use-module (gnu packages web)
   #:use-module (gnu packages python)
   #:use-module (gnu packages python-web)
+  #:use-module (gnu packages python-crypto)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages time)
+  #:use-module (gnu packages databases)
   #:use-module (gnu packages check)
   #:use-module (gn packages web)
   #:use-module (gn packages python))
@@ -87,6 +89,50 @@ interface to the Amazon Web Services (AWS) API.")
   (description "The AWS SDK for Python")
   (license license:asl2.0)))
 
+(define-public python-speaklater
+  (package
+  (name "python-speaklater")
+  (version "1.3")
+  (source
+    (origin
+      (method url-fetch)
+      (uri (pypi-uri "speaklater" version))
+      (sha256
+        (base32
+          "1ab5dbfzzgz6cnz4xlwx79gz83id4bhiw67k1cgqrlzfs0va7zjr"))))
+  (build-system python-build-system)
+  (home-page
+    "http://github.com/mitsuhiko/speaklater")
+  (synopsis
+    "implements a lazy string for python useful for use with gettext")
+  (description
+    "implements a lazy string for python useful for use with gettext")
+  (license license:bsd-3)))
+
+(define-public python-flask-mail
+  (package
+   (name "python-flask-mail")
+   (version "0.9.1")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (pypi-uri "flask-mail" version))
+     (sha256
+      (base32
+       "0hazjc351s3gfbhk975j8k65cg4gf31yq404yfy0gx0bjjdfpr92"))))
+   (build-system python-build-system)
+   (propagated-inputs
+    `(("python-mock" ,python-mock)
+      ("python-nose" ,python-nose)
+      ("python-flask" ,python-flask)
+      ("python-blinker" ,python-blinker)
+      ("python-speaklater" ,python-speaklater)))
+   (home-page
+    "https://github.com/rduplain/flask-mail")
+   (synopsis "Flask extension for sending email")
+   (description "Flask extension for sending email")
+   (license license:bsd-3)))
+
 (define-public edash
   (let ((md5 "93e745e9c"))
     (package
@@ -116,7 +162,14 @@ interface to the Amazon Web Services (AWS) API.")
        ("web-bootstrap" ,web-bootstrap)
        ;;("sassc" ,sassc)
        ("python-boto3" ,python-boto3)
-       ("python-pytest" ,python-pytest)))
+       ("python-pytest" ,python-pytest)
+       ("python-flask-login" ,python-flask-login)
+       ("python-passlib" ,python-passlib)
+       ("python-pyjwt" ,python-pyjwt)
+       ("python-sqlalchemy" ,python-sqlalchemy)
+       ("python-flask-sqlalchemy" ,python-flask-sqlalchemy)
+       ("python-flask-mail" ,python-flask-mail)
+       ("python-flask-wtf" ,python-flask-wtf)))
     (arguments
      `(#:modules ((guix build utils))
        #:builder
