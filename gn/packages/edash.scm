d91eb394eb9e9eaf05dbb38845fd1abe6acbcb45
pjotrp <pjotr.public01@thebird.nl>
2018-11-03 12:52:32 +0000

    Add sqlite

--- gn/packages/edash.scm
+++ gn/packages/edash.scm
@@ -169,7 +169,9 @@ interface to the Amazon Web Services (AWS) API.")
        ("python-sqlalchemy" ,python-sqlalchemy)
        ("python-flask-sqlalchemy" ,python-flask-sqlalchemy)
        ("python-flask-mail" ,python-flask-mail)
-       ("python-flask-wtf" ,python-flask-wtf)))
+       ("python-flask-wtf" ,python-flask-wtf)
+       ("sqlite" ,sqlite)
+       ))
     (arguments
      `(#:modules ((guix build utils))
        #:builder
