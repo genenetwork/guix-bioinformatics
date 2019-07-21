a8703d14fbee518a8e7dabf0df8a97f19e69e4f1
Efraim Flashner <efraim@flashner.co.il>
2019-07-21 02:19:39 -0500

    gn: web-bootstrap-native: Clean up package.
    * gn/packages/web.scm (web-bootstrap-native): Re-indent.
    [arguments]: Clean up build.

--- gn/packages/web.scm
+++ gn/packages/web.scm
@@ -82,38 +82,34 @@ extensive prebuilt components, and powerful plugins built on jQuery.")
 
 (define-public web-bootstrap-native
   (let ((commit "2e48d7ee29d4063e3bd2024ff83ddc50a550c4dd"))
-  (package
-    (name "web-bootstrap-native")
-    (version (string-append "4.0.0" "-beta-" (string-take commit 7)))
-    (source (origin
-             (method git-fetch)
-             (uri (git-reference
-                   (url "https://github.com/thednp/bootstrap.native.git")
-                   (commit commit)))
-             (file-name (string-append name "-" commit))
-             (sha256
-              (base32
-               "1hkyibyfby0mnkavr3xbmr20kb88wy6jw28b206pd236xnp2qkx0"))))
-    (build-system trivial-build-system)
-    (native-inputs `(("source" ,source)))
-    (arguments
-     `(#:modules ((guix build utils))
-       #:builder
-       (let* ((out (assoc-ref %outputs "out"))
-              (name "bootstrap-native")
-              (targetdir (string-append out "/share/web/" name))
-              )
+    (package
+      (name "web-bootstrap-native")
+      (version (string-append "4.0.0" "-beta-" (string-take commit 7))) ; Sept, 8, 2017
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                       (url "https://github.com/thednp/bootstrap.native.git")
+                       (commit commit)))
+                (file-name (string-append name "-" commit))
+                (sha256
+                 (base32
+                  "1hkyibyfby0mnkavr3xbmr20kb88wy6jw28b206pd236xnp2qkx0"))))
+      (build-system trivial-build-system)
+      (arguments
+       `(#:modules ((guix build utils))
+         #:builder
+         (let* ((out (assoc-ref %outputs "out"))
+                (name "bootstrap-native")
+                (targetdir (string-append out "/share/web/" name))
+                (source (assoc-ref %build-inputs "source")))
            (begin
              (use-modules (guix build utils))
-             (let ((source (assoc-ref %build-inputs "source")))
-               (and
-                    ; (mkdir-p targetdir)
-                    (copy-recursively source targetdir)
-                    ))))))
-    (home-page "https://github.com/thednp/bootstrap.native")
-    (synopsis "Bootstrap minimal")
-    (description "Bootstrap native does not use jquery.")
-    (license license:expat))))
+             (copy-recursively source targetdir)))))
+      (native-inputs `(("source" ,source)))
+      (home-page "https://github.com/thednp/bootstrap.native")
+      (synopsis "Bootstrap minimal")
+      (description "Bootstrap native does not use jquery.")
+      (license license:expat))))
 
 (define-public mod-python
   (let ((commit "902bb8700e2c45ffd96b78e2f1146a3c101be7f5")
