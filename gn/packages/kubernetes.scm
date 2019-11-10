fc831aba237cc72fc2e5d5083977fb96d098689a
Efraim Flashner <efraim@flashner.co.il>
2019-11-10 05:59:50 -0600

    gn: Add minikube.

--- gn/packages/kubernetes.scm
+++ gn/packages/kubernetes.scm
@@ -2,7 +2,9 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix git-download)
+  #:use-module (guix download)
   #:use-module (guix build-system go)
+  #:use-module (guix build-system trivial)
   #:use-module (gnu packages base)
   #:use-module (gnu packages rsync)
   )
@@ -90,4 +92,38 @@
     (description "Kubernetes is an open source system for managing containerized
 applications across multiple hosts.  It provides basic mechanisms for
 deployment, maintenance, and scaling of applications.")
+    (license license:asl2.0)))
+
+(define-public minikube
+  (package
+    (name "minikube")
+    (version "1.5.2")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://github.com/kubernetes/minikube/"
+                                  "releases/download/v"
+                                  version "/" name "-linux-amd64"))
+              (sha256
+               (base32
+                "1sgpb5k3i6g1slz9f6lvp4br5llgm2wcklpn2804hpp8dnlsjwhr"))))
+    (build-system trivial-build-system)
+    (arguments
+     `(#:modules ((guix build utils))
+       #:builder
+       (begin
+         (use-modules (guix build utils))
+         (let* ((out     (assoc-ref %outputs "out"))
+                (bin     (string-append out "/bin"))
+                (target  (string-append bin "/minikube"))
+                (source  (assoc-ref %build-inputs "source")))
+           (mkdir-p bin)
+           (copy-file source target)
+           (chmod target #o555))
+         #t)))
+    (home-page "https://minikube.sigs.k8s.io/")
+    (synopsis "Run Kubernetes locally")
+    (description "Minikube implements a local Kubernetes cluster.  Minikube's
+primary goals are to be the best tool for local Kubernetes application
+development and to support all Kubernetes features that fit.")
+    (supported-systems '("x86_64-linux"))
     (license license:asl2.0)))
