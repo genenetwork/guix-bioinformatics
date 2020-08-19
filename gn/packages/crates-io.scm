5f93809c646dc8bb93ee6d23b9046751314f7a04
Efraim Flashner <efraim@flashner.co.il>
2020-08-19 03:39:00 -0500

    gn: Add gfautil.

--- gn/packages/crates-io.scm
+++ gn/packages/crates-io.scm
@@ -5,9 +5,81 @@
   #:use-module (guix download)
   #:use-module (guix utils)
   #:use-module (guix build-system cargo)
-  #:use-module (gnu packages crates-io))
+  #:use-module (gnu packages crates-io)
+  #:use-module (gnu packages rust))
 
 ;; Please keep these packages sorted alphabetically
+
+(define-public rust-bytemuck-1.3
+  (package
+    (inherit rust-bytemuck-1)
+    (name "rust-bytemuck")
+    (version "1.3.1")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "bytemuck" version))
+        (file-name
+         (string-append name "-" version ".tar.gz"))
+        (sha256
+         (base32
+          "1scaac5xbfynzbpvz9yjbmg9ag2jalxfijapwlqh7xldf4li0ynv"))))))
+
+(define-public rust-handlegraph-0.3
+  (package
+    (name "rust-handlegraph")
+    (version "0.3.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "handlegraph" version))
+        (file-name
+         (string-append name "-" version ".tar.gz"))
+        (sha256
+         (base32
+          "1sj100w4lpj7798pws85qrfrzsily5hhzh6j118rwf56sgic1yml"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:rust ,rust-1.42
+       #:cargo-inputs
+       (("rust-bstr" ,rust-bstr-0.2)
+        ("rust-gfa" ,rust-gfa-0.6))))
+    (home-page "https://github.com/chfi/rs-handlegraph")
+    (synopsis "Library for use in variation graphs")
+    (description
+     "This package provides a Rust implementation of VG handle graph.")
+    (license license:expat)))
+
+(define-public rust-gfa-0.6
+  (package
+    (name "rust-gfa")
+    (version "0.6.2")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "gfa" version))
+        (file-name
+         (string-append name "-" version ".tar.gz"))
+        (sha256
+         (base32
+          "0ghmy4r0324s6vvmj9nmh326346nkwm7nybnpcpswnjvf02b85gw"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-bstr" ,rust-bstr-0.2)
+        ("rust-bytemuck" ,rust-bytemuck-1.3)
+        ("rust-lazy-static" ,rust-lazy-static-1)
+        ("rust-nom" ,rust-nom-5)
+        ("rust-regex" ,rust-regex-1)
+        ("rust-serde" ,rust-serde-1))
+       #:cargo-development-inputs
+       (("rust-criterion" ,rust-criterion-0.3))))
+    (home-page "https://github.com/chfi/rs-gfa")
+    (synopsis "Library for graphs in the GFA (Graphical Fragment Assembly) format")
+    (description
+     "This package provides a library for working with graphs in the
+@acronym{GFA, Graphical Fragment Assembly} format.")
+    (license license:expat)))
 
 (define-public rust-ndarray-0.12
   (package
--- gn/packages/genenetwork.scm
+++ gn/packages/genenetwork.scm
@@ -25,6 +25,7 @@
   #:use-module (gnu packages python-science)
   #:use-module (gnu packages python-web)
   #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages rust)
   #:use-module (gnu packages statistics)
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages vim)
@@ -281,6 +282,37 @@ implemented, light on server resource usage, and fairly speedy.")
       (synopsis "Reimplementation of genenetwork/QTLReaper in Rust")
       (description "Reimplementation of genenetwork/QTLReaper in Rust")
       (license #f))))
+
+(define-public gfautil
+  (package
+    (name "gfautil")
+    (version "0.1.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "gfautil" version))
+        (file-name
+         (string-append name "-" version ".tar.gz"))
+        (sha256
+         (base32
+          "0cgiis9v1nd4m7zxvgsz8jf8ijv4d8fa5wb7cpnjshksb8z7xh69"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:rust ,rust-1.42
+       #:cargo-inputs
+       (("rust-bstr" ,rust-bstr-0.2)
+        ("rust-clap" ,rust-clap-2)
+        ("rust-gfa" ,rust-gfa-0.6)
+        ("rust-handlegraph" ,rust-handlegraph-0.3)
+        ("rust-rayon" ,rust-rayon-1)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-structopt" ,rust-structopt-0.3))))
+    (home-page "https://github.com/chfi/rs-gfa-utils")
+    (synopsis "Command line tools for working with GFA files")
+    (description
+     "This package provides command line tools for working with @acronym{GFA,
+Graphical Fragment Assembly} files and related formats.")
+    (license license:expat)))
 
 (define-public genenetwork2
   (let ((commit "1538ffd33af19e6ac922b4ee85fe701408968dfd"))
